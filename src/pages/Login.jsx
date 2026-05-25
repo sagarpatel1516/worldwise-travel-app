99e4c3b7bf558b41d2783deb88f3752a015f35f6
sagarpatel1516 <sagarpatel2524@gmail.com>
2026-05-25 12:22:48 +0530

    Fix import path for useAuth in Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/fakeAUthContext.jsx";
+import { useAuth } from "../contexts/fakeAuthContext.jsx";
 import Button from "../components/Button.jsx";
 
 export default function Login() {
